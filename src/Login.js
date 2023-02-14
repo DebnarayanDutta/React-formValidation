a94f3831a8bdd63e21a52426537dcafe3c5244a8
Debnarayan <debnarayanduttaz663@gmail.com>
2023-02-14 21:39:16 +0530

    second commit

---
+++ src/Login.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react'
+
+function Login(){
+    const [user,setUser] = useState("");
+    const [password,setPassword] = useState("");
+    const [userErr,setUserErr] = useState(false);
+    const [passErr,setPassErr]=useState(false);
+
+    function loginHandle(e){
+
+        if(user.length<3 || password.length<3){
+            alert("type correct values")
+        }
+        else
+        {
+            alert("all good")
+        }
+
+      e.preventDefault()
+    }
+    function userHandler(e){
+        let item = e.target.value;
+        if(item.length<3){
+            setUserErr(true)
+        }
+        else{
+            setUserErr(false)
+        }
+        setUser(item)
+    }
+    function passwordHandler(e){
+        let item= e.target.value;
+        if(item.length<3){
+            setPassErr(true)
+        }
+        else{
+            setPassErr(false)
+        }
+        setPassword(item)
+    }
+    return(
+        <div>
+            <h1>Login</h1>
+            <form onSubmit ={loginHandle}>
+                <input type="text" placeholder="Enter user id" onChange={userHandler}/>{userErr?<span>User not valid</span>:""}
+                <br/> <br/>
+                <input type="password" placeholder="Enter user Pasword" onChange={passwordHandler}/>{passErr?<span>Password not valid</span>:""}
+                <br/> <br/>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
